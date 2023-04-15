bea22e91b5a899d6947668db5cdcca3371d47eca
Rrayor <simon.benjamin93@gmail.com>
2023-04-15 12:42:56 +0200

    reading json files - NOT parsing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, error::Error, io::Read};
+use std::{fs::File, error::Error, io::Read, path::Path};
 
 #[derive(Debug)]
 struct DataObject<'a> {
@@ -14,9 +14,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
         children: vec![]
     };
     println!("{}", format!("Test Dataobject: {test_do:?}"));
-    let test_file_content = read_file("test_file.json")?;
-    println!("Test file contents:\n{}", &test_file_content);
+    let test_json_file_content = read_json("test_file.json")?;
+    println!("Test file contents:\n{}", &test_json_file_content.trim());
+    let test_txt_file_content = read_json("test_file.txt")?;
+    println!("Test file contents:\n{}", &test_txt_file_content.trim());
     Ok(())
+}
+
+fn read_json(file_name: &str) -> Result<String, Box<dyn Error>> {
+    let extension_option = Path::new(file_name)
+        .extension()
+        .and_then(|ext| ext.to_str());
+
+    if extension_option != Some("json") {
+        return Err(
+            format!(
+                "Expected file extension to be '.json' on file: {}!",
+                file_name
+            ).into()
+        );
+    }
+
+    read_file(file_name)
 }
 
 fn read_file(file_name: &str) -> Result<String, Box<dyn Error>> {
