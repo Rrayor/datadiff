7f82ccc4aee3b456281d728960c1fbd61e0c939d
Rrayor <simon.benjamin93@gmail.com>
2023-04-17 09:52:43 +0200

    refactor to use serde and started working on comparison algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,53 @@
-use std::{fs::File, error::Error, io::Read, path::Path};
+use std::{fs::File, collections::HashMap};
+use std::io::BufReader;
+use serde_json::{Result, Value};
 
-#[derive(Debug)]
-struct DataNode<'a> {
-    name: &'a str,
-    value: Option<&'a str>,
-    children: Vec<DataNode<'a>>
+struct KeyDiff<'a> {
+    key: &'a str,
+    has: &'a str,
+    misses: &'a str
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
-    let test_do = DataNode {
-        name: "donnow",
-        value: Some("something"),
-        children: vec![]
-    };
-    println!("{}", format!("Test Dataobject: {test_do:?}"));
-    let test_json_file_content = read_json("test_file.json")?;
-    println!("Test file contents:\n{}", &test_json_file_content.trim());
-    let test_txt_file_content = read_json("test_file.txt")?;
-    println!("Test file contents:\n{}", &test_txt_file_content.trim());
+struct ValueDiff<'b> {
+    key: &'b str,
+    value1: (&'b str, &'b str),
+    value2: (&'b str, &'b str)
+}
+
+fn main() -> Result<()> {
+    let data1 = read_json_file("test_data/person1.json")?;
+    let data2 = read_json_file("test_data/person2.json")?;
+    println!("data1: {:?}", data1);
+    println!("data2: {:?}", data2);
     Ok(())
 }
 
-fn read_json(file_name: &str) -> Result<String, Box<dyn Error>> {
-    let extension_option = Path::new(file_name)
-        .extension()
-        .and_then(|ext| ext.to_str());
-
-    if extension_option != Some("json") {
-        return Err(
-            format!(
-                "Expected file extension to be '.json' on file: {}!",
-                file_name
-            ).into()
-        );
-    }
-
-    read_file(file_name)
+fn read_json_file(file_path: &str) -> Result<Value> {
+    let file = File::open(file_path)
+        .expect(&format!("Could not open file {}", file_path));
+    let reader = BufReader::new(file);
+    let result = serde_json::from_reader(reader)?;
+    Ok(result)
 }
 
-fn read_file(file_name: &str) -> Result<String, Box<dyn Error>> {
-    let mut file = File::open(file_name)
-        .map_err(
-            |e| format!("Unable to open file {}: {}", file_name, e)
-        )?;
-    let mut file_contents = String::new();
-    file
-        .read_to_string(&mut file_contents)
-        .map_err(
-            |e| format!("Unable to read contents of file {}: {}", file_name, e)
-        )?;
-    Ok(file_contents)
+fn compare_objects(a: &Value, b: &Value) -> (Vec<KeyDiff>, Vec<ValueDiff>) {
+    match(a, b) {
+        (Value::Null, Value::Null) => (vec![], vec![]),
+        (Value::Bool(a), Value::Bool(b)) => {
+            let key_diff = vec![];
+            let mut value_diff: Vec<ValueDiff> = vec![];
 
+            if a != b {
+                value_diff = vec![
+                    ValueDiff {
+                        key: "",
+                        value1: ("", a.to_string().as_str()),
+                        value2: ("", b.to_string().as_str())
+                    }
+                ];
+            }
+
+            return (key_diff, value_diff);
+        } 
+    }
 }
